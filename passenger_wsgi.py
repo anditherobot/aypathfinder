3933581f230c93b75a5a73129c28bf898f223bae
andi <andi@anditherobot.com>
2021-02-09 14:49:48 -0500

    added it

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,1 @@
-
-import os
-import sys
-
-
-#sys.path.insert(0, os.path.dirname(__file__))
-
-#wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-#application = wsgi.application
-
 from aycore.wsgi import application
-sys.path.insert(0, os.path.dirname(__file__))
